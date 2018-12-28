2aa20919680488c578cc8aacee4b9db2e2a26db7
Vlodson <vladimir.lunic2000@gmail.com>
2018-12-28 03:49:18 +0100

    dodate jos 2 metode u klasi. to je uradjeno u 3:40 ujutru upitno je kolko valja

--- Celija.py
+++ Celija.py
@@ -17,14 +17,33 @@ import numpy as np
 class Celija():
 
     """ da bi napravio objekat moras dati x i y pocetnu koordinatu za celiju """
+
     def __init__(self, pocetna_pozicija_x, pocetna_pozicija_y):
         self.pocetna_pozicija_y = pocetna_pozicija_x
         self.pocetna_pozicija_x = pocetna_pozicija_y
 
     """ pocetna pozicija celije, x i y ce da se menjaju """
+
     def pocetna_pozicija(self):
         # return print("pozicija:", self.pocetna_pozicija_x, self.pocetna_pozicija_y)
         return (self.pocetna_pozicija_x, self.pocetna_pozicija_y)
+
+    """ pomeranje celije: euklidska distanca do osvetljenog polja (da vidi da li moze da stigne do tamo a da ne umre)
+        i gledanje da li je (x,y) osvetljenog vece ili manje od (x,y) celije onda +-1 na x y osi putem toga """
+
+    def pomeraj(self, matrica_svet):
+
+
+    """ smrt celije: ako celija nije na osvetljenom x sati umire (skontati kolko je x, za sada je 3 jer testiram) """
+
+    def smrt(self, matrica_svet):
+        counter = 0
+        while self.pozicija()[0][1] == 1: # ovde ce biti i metoda pozicija koja kaze gde se trenutno nalazi celija i ako su trenutne koordinate celije 1 onda je celija ziva i ova metoda moze da ide
+            if matrica_svet[self.pozicija()[0]][self.pozicija()[1]] == 0:
+                counter += 1
+
+            if counter == 3:
+                self.pozicija()[0][1] == 0
 
     # def test(self,other):
     #     return self.pocetna_pozicija[0] + other.pocetna_pozicija[0], self.pocetna_pozicija[1] + other.pocetna_pozicija[1]
@@ -37,23 +56,29 @@ class Celija():
 # c2 = Celija(1,1)
 
 """ fabrika celija """
+
 celije = []
 for i in range(3):
-    celije.append(Celija(i,i))
+    celije.append(Celija(np.random.randint(0,9),np.random.randint(0,9))) # mozda se nekada poklope, trenutno je 3:41 mrzi me jako da ispisem jos 2 linije da se to sredi
 
 
-""" test svet (10x10) (u njemu i spavnujem celije)"""
+""" svet celija (10x10) (u njemu i spavnujem celije)"""
 
-svet = np.zeros((10,10), np.int8)
+svet_celija = np.zeros((10,10), np.int8)
 
 """ Celija.pocetna_pozicija() ima dva elementa pa na tim mestima u svetu se nalazi celija koja je obelezena sa 2 """
+
 def spavnovanje(matrica, celija):
-    matrica[celija.pocetna_pozicija()[0]][celija.pocetna_pozicija()[1]] = 2
+    matrica[celija.pocetna_pozicija()[0]][celija.pocetna_pozicija()[1]] = 1
 
 """ prolazim kroz listu celija i spawnjujem ih """
 for i in range(len(celije)):
-    spavnovanje(svet, celije[i])
+    spavnovanje(svet_celija, celije[i])
 
 # svet[c1.pocetna_pozicija()[0]][c1.pocetna_pozicija()[1]] = 2
 
 #------------------------------------
+
+""" fizicki svet (u kom je dan noc ciklus i sve to) """
+
+fizicki_svet = np.zeros((10,10), np.int8)
